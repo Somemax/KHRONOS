2e2d11d3b1f0d9dd8ee27499a1bf96b58beab39c
Zeguang LI <lizeguang@mail.tsinghua.edu.cn>
2013-11-21 19:18:14 +0800

    Initial Commit

---
+++ KHRONOS/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  KHRONOS
+//
+//  Created by LI Zeguang on 11/21/13.
+//  Copyright (c) 2013 THU. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
